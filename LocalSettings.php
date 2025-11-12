c7e33836ac8055120d92791f1495d6071bcae27c
steff.pro <steff.pro@bluewin.ch>
2025-11-12 19:07:50 +0100

    Add Wikibase extensions and example settings to LocalSettings.php

--- LocalSettings.php
+++ LocalSettings.php
@@ -224,6 +224,9 @@ $wgJsonConfigs['Map.JsonConfig']['remote'] = [
 #=============================================================
 
 # Wikibase
+wfLoadExtension( 'WikibaseRepository', "$IP/extensions/Wikibase/extension-repo.json" );
+require_once "$IP/extensions/Wikibase/repo/ExampleSettings.php";
+
 wfLoadExtension( 'WikibaseClient', "$IP/extensions/Wikibase/extension-client.json" );
 require_once "$IP/extensions/Wikibase/client/ExampleSettings.php";
 
